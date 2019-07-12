6e3c49fd8d741bae64a473885b3b8a75470712f2
Dasha6elka <CB4kot@gmail.com>
2019-07-12 14:54:55 +0300

    Редактирование

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,6 +6,7 @@ import ListIcon from "../icons/ListIcon";
 import TrashIcon from "../icons/TrashIcon";
 import PropTypes from "proptypes";
 import Input from "@material-ui/core/Input";
+import Grid from "@material-ui/core/Grid";
 
 function ListItem(props) {
   function onChange(event) {
@@ -34,17 +35,20 @@ function ListItem(props) {
     margin: "0",
     marginLeft: "16px",
     minWidth: "0",
-    maxHeight: "24px"
+    maxHeight: "24px",
+    maxWidth: "160px"
   };
 
   return (
     <React.Fragment>
-      <div
+      <Grid
+        container
+        direction="row"
+        justify="flex-start"
+        alignItems="center"
         css={css`
-          display: flex;
-          align-items: center;
           color: ${props.button ? "grey" : "inherit"};
-          padding: 8px 8px 8px 8px;
+          padding: 8px;
           position: relative;
           cursor: ${props.button ? "text" : "pointer"};
           font-size: 14px;
@@ -75,6 +79,9 @@ function ListItem(props) {
             css={css`
               flex-grow: 1;
               margin-left: 16px;
+              text-overflow: ellipsis;
+              overflow: hidden;
+              max-width: 160px;
             `}
             onClick={props.button && props.onClick}
           >
@@ -84,7 +91,7 @@ function ListItem(props) {
           <Input className="input" onChange={onChange} value={props.title} style={inputStyle} />
         )}
         {!props.button && <TrashIcon className="trash-icon" onClick={props.onDelete} />}
-      </div>
+      </Grid>
       {errorMessage(props)}
     </React.Fragment>
   );
@@ -117,3 +124,50 @@ function errorMessage(props) {
     </div>
   );
 }
+
+/*<div
+  css={css`
+          display: flex;
+          align-items: center;
+          color: ${props.button ? "grey" : "inherit"};
+          padding: 8px 8px 8px 8px;
+          position: relative;
+          cursor: ${props.button ? "text" : "pointer"};
+          font-size: 14px;
+          line-height: 16px;
+
+          .trash-icon {
+            display: none;
+          }
+
+          &:hover {
+            background: rgba(59, 165, 209, 0.15);
+            color: #83c6e2;
+
+            .list-icon path {
+              fill: #83c6e2;
+            }
+
+            .trash-icon {
+              display: block;
+              margin-left: 16px;
+            }
+          }
+        `}
+>
+  <ListIcon className="list-icon" />
+  {!props.editable ? (
+    <div
+      css={css`
+              flex-grow: 1;
+              margin-left: 16px;
+            `}
+      onClick={props.button && props.onClick}
+    >
+      {!props.button ? `${props.title} (${props.count})` : `${props.title}`}
+    </div>
+  ) : (
+    <Input className="input" onChange={onChange} value={props.title} style={inputStyle} />
+  )}
+  {!props.button && <TrashIcon className="trash-icon" onClick={props.onDelete} />}
+</div>*/
