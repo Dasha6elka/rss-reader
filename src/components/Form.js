10638376f11aa4c66e413d65166daaab129e8b41
Dasha6elka <CB4kot@gmail.com>
2019-07-16 11:15:43 +0300

    Изменение формы добавления RSS-ленты

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,13 @@
 /** @jsx jsx */
 
-import { jsx } from "@emotion/core";
-import useForm from "./useForm";
+import { jsx, css } from "@emotion/core";
+import useForm from "../hooks/useForm";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
-import Input from "@material-ui/core/Input";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import { Button, createMuiTheme } from "@material-ui/core";
 import { MuiThemeProvider } from "@material-ui/core/styles";
-
+import Input from "./Input";
 
 const theme = createMuiTheme({
   palette: {
@@ -17,7 +16,6 @@ const theme = createMuiTheme({
 });
 
 function Form(props) {
-
   const stateSchema = {
     name: { value: "", error: "" },
     link: { value: "", error: "" },
@@ -67,65 +65,122 @@ function Form(props) {
     marginTop: "16px"
   };
 
-  const labelStyle = {
-    color: "#fff"
-  };
-
   return (
     <form onSubmit={handleOnSubmit}>
       <FormControl fullWidth={true}>
-        <InputLabel htmlFor="name" style={labelStyle}>
+        <InputLabel
+          css={css`
+            color: white !important;
+          `}
+          htmlFor="name"
+        >
           Имя ленты
         </InputLabel>
         <Input
           type="text"
           name="name"
+          autoComplete="off"
           id="name"
           onChange={handleOnChange}
           value={state.name.value}
           required
+          css={css`
+            margin-left: 0;
+            margin-bottom: 0;
+            color: white;
+
+            :hover:before {
+              border-bottom: 2px solid white !important;
+            }
+          `}
         />
         {state.name.error && <FormHelperText style={errorStyle}>{state.name.error}</FormHelperText>}
       </FormControl>
-      <FormControl fullWidth={true}>
-        <InputLabel htmlFor="link" style={labelStyle}>
+      <FormControl
+        fullWidth={true}
+        css={css`
+          margin-top: 16px !important;
+        `}
+      >
+        <InputLabel
+          htmlFor="link"
+          css={css`
+            color: white !important;
+          `}
+        >
           Ссылка на RRS
         </InputLabel>
         <Input
           type="text"
           name="link"
+          autoComplete="off"
           id="link"
           onChange={handleOnChange}
           value={state.link.value}
           required
+          css={css`
+            margin-left: 0;
+            margin-bottom: 0;
+            color: white;
+
+            :hover:before {
+              border-bottom: 2px solid white !important;
+            }
+          `}
         />
         {state.link.error && <FormHelperText style={errorStyle}>{state.link.error}</FormHelperText>}
       </FormControl>
-      <FormControl fullWidth={true}>
-        <InputLabel htmlFor="category" style={labelStyle}>
+      <FormControl
+        fullWidth={true}
+        css={css`
+          margin-top: 16px !important;
+        `}
+      >
+        <InputLabel
+          htmlFor="category"
+          css={css`
+            color: white !important;
+          `}
+        >
           Категория
         </InputLabel>
         <Input
           type="text"
           name="category"
+          autoComplete="off"
           id="category"
           onChange={handleOnChange}
           value={state.category.value}
           required
+          css={css`
+            margin-left: 0;
+            margin-bottom: 0;
+            color: white;
+
+            :hover:before {
+              border-bottom: 2px solid white !important;
+            }
+          `}
         />
         {state.category.error && <FormHelperText style={errorStyle}>{state.category.error}</FormHelperText>}
       </FormControl>
       <MuiThemeProvider theme={theme}>
         <Button
+          css={css`
+            :disabled {
+              color: #3ba5d1;
+              background-color: rgba(59, 165, 209, 0.08);
+            }
+          `}
           style={buttonStyle}
           name="submit"
           disabled={disable}
           onClick={props.onClick}
-          StyledButton
           type="submit"
           href=""
           variant="text"
           color="primary"
+          classes={{ disabled: "disabled" }}
         >
           Добавить
         </Button>
