1a7fb04711f0dacd7ef2f0a97288b9588bcf7f01
Dasha6elka <CB4kot@gmail.com>
2019-07-21 19:44:41 +0300

    Установление активной категории и количсетво лент

--- src/components/List.js
+++ src/components/List.js
@@ -6,13 +6,26 @@ import { Grid } from "@material-ui/core";
 import ListItem from "./ListItem";
 
 function List(props) {
-  const { data, onChange, onFinish, onDelete } = props;
+  const { data, onChange, onFinish, onDelete, activeCategory, setActiveCategory, channels } = props;
 
   function onButtonClick() {
     if (data.some(value => value.editable === true)) {
       return;
     }
-    onChange([...data, { title: "", editable: true, count: 0 }]);
+    onChange([...data, { title: "", editable: true, active: false, count: 0 }]);
+  }
+
+  function onClick(id) {
+    data.forEach(item => {
+      if (item.id === id) {
+        item.active = true;
+      }
+      if (item.id === activeCategory) {
+        item.active = false;
+      }
+    });
+    onChange([...data]);
+    setActiveCategory(id);
   }
 
   function onListItemChange(event, index) {
@@ -35,27 +48,39 @@ function List(props) {
     onChange([...data]);
   }
 
+  function getCount(value) {
+    let count = 0;
+    channels.forEach(channel => {
+      if (channel.id_category === value.id) {
+        count++;
+      }
+    });
+    return count;
+  }
+
   return (
     <React.Fragment>
-      <div
+      <Grid
         css={css`
+          width: 100%;
           border: 1px solid #0a0a0a;
           margin: 12px 0;
         `}
       />
 
-      <Grid container direction="column" justify="center" alignItems="stretch">
+      <Grid container direction="column" spacing={2}>
         {data.map((value, index) => (
           <ListItem
             key={index}
             title={value.title}
-            count={value.count}
+            count={getCount(value)}
             editable={value.editable}
             active={value.active}
             onChange={event => onListItemChange(event, index)}
             onEditFinish={onListItemEditFinish}
             onDelete={() => onListItemDelete(value.id)}
             errorMessage={value.error}
+            onListClick={() => onClick(value.id)}
           />
         ))}
         <ListItem button title="Новая категория" onClick={onButtonClick} />
