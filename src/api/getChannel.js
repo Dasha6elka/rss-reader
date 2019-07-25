fb508adaf7ca4073bf5cb112f5325e1ba9cfd648
Dasha6elka <CB4kot@gmail.com>
2019-07-25 11:57:42 +0300

    Удаление неиспользуемого файла

--- src/api/getChannel.js
+++
@@ -1,8 +0,0 @@
-import { API_URL } from "../constants";
-
-async function getChannel(url) {
-  const response = await fetch(`${API_URL}/fetch/${url}`);
-  return response.text();
-}
-
-export default getChannel;
