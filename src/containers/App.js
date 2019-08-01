21dc01cfc165d695d3b4c579a206d40467d3b963
Dasha6elka <CB4kot@gmail.com>
2019-08-01 12:21:47 +0300

    Изменнеие

--- src/containers/App.js
+++ src/containers/App.js
@@ -92,6 +92,7 @@ function App() {
   }
 
   function onChannelsEditFinish(channelId) {
+    onLoadingLogoUrlChange(true);
     channels.forEach(channel => {
       parser.parseURL(CORS_PROXY + channel.rssUrl).then(feed => {
         if (channel.id === channelId) {
